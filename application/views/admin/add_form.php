937c1baa1e3d7d2c64adb6f0a72bf89dd760a7d5
jashimgit <jashim.ncn@gmail.com>
2020-12-07 11:24:56 +0600

    add form added

--- application/views/admin/add_form.php
+++ application/views/admin/add_form.php
@@ -1,11 +1,14 @@
+<br>
 <div class="container">
+    <div class="card">
+        <div >
+            <h2 >Add UDC</h2>
+        </div>
 
-
-
-<div class="row">
-    <form action="">
+        <div class="card-body">
+            <form action="">
         <div class="form-row">
-            <div class="col">
+            <div class="form-group col-md-3">
                 <select name="" id="" class="form-control">
                     <option value="">Select Division</option>
                     <option value="1">Dhaka</option>
@@ -13,12 +16,66 @@
                     <option value="1">Khulna</option>
                 </select>
             </div>
-            <div class="col">
-                <input type="text" class="form-control" placeholder="Last name">
+            <div class="form-group col-md-3">
+                <select name="" id="" class="form-control">
+                    <option value="">Select District</option>
+                    <option value="1">Dhaka</option>
+                    <option value="1">Chittagong</option>
+                    <option value="1">Khulna</option>
+                </select>
+            </div>
+            <div class="form-group col-md-3">
+                <select name="" id="" class="form-control">
+                    <option value="">Select Upzila</option>
+                    <option value="1">Dhaka</option>
+                    <option value="1">Chittagong</option>
+                    <option value="1">Khulna</option>
+                </select>
+            </div>
+            <div class="form-group col-md-3">
+                <select name="" id="" class="form-control">
+                    <option value="">Select Union</option>
+                    <option value="1">Dhaka</option>
+                    <option value="1">Chittagong</option>
+                    <option value="1">Khulna</option>
+                </select>
             </div>
         </div>
+        <div class="form-group row">
+            <label  class="col-sm-2 col-form-label">Name</label>
+            <div class="col-sm-10">
+              <input type="text" class="form-control" id="name">
+          </div>
+        </div>
+        
+        <div class="form-row">
+             <div class="form-group col-md-4">
+                 <label>phone 1</label>
+                 <input type="text" class="form-control" name="">
+             </div>
+             <div class="form-group col-md-4">
+                 <label>phone 2</label>
+                 <input type="text" class="form-control" name="">
+             </div>
+             <div class="form-group col-md-4">
+                 <label>phone 3</label>
+                 <input type="text" class="form-control" name="">
+             </div>
+        </div>
+         
+        <div class="form-group row">
+            <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+            <div class="col-sm-10">
+              <input type="email" class="form-control" id="inputEmail3">
+          </div>
+        </div>
+         <div class="form-group  ">
+             <label>remarks</label>
+             <textarea class="form-control" cols="4" rows="4"></textarea>
+         </div>
 
-    </form>
-</div>
-
+         <button class="btn btn-success" type="submit">Save</button>
+    </form> <!-- // form -->
+        </div>
+    </div>
 </div>
